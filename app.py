13be4326fcfc2a1f8faacce23f97ded360018fc4
celis <acelisalas@gmail.com>
2020-01-15 21:33:22 +0100

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template
 from gensim.models import KeyedVectors
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
@@ -17,13 +17,44 @@ class InputForm(FlaskForm):
     submit = SubmitField('Submit')
 
 
+class GensimWrapper:
+    """
+    """
+
+    def __init__(self):
+        """
+        :param
+        """
+        self._model = None
+
+    def load(self, model_path):
+        self._model = KeyedVectors.load_word2vec_format(model_path, binary=True)
+
+    def most_similar(self, article: str, topn: int = 5):
+        """
+        """
+        return [article[0] for article in self._model.similar_by_word(article, topn)]
+
+    def vocabulary(self):
+        """
+        Returns all record ids which have embeddings
+        """
+        return [recid for recid in self._model.vocab]
+
+model = GensimWrapper()
+model.load("model/article_embeddings.bin")
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     id = None
+    recommendations = None
     form = InputForm()
     if form.validate_on_submit():
         id = form.id.data
-    return render_template('index.html', form=form, id=id)
+        if id in model.vocabulary():
+            recommendations = model.most_similar(id)
+    return render_template('index.html', form=form, id=id, recommendations=recommendations)
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
