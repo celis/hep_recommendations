2d4385d1a90a0ef7e641396c699ecee8dedb7579
celis <acelisalas@gmail.com>
2020-02-03 13:47:46 +0100

    add logging gunicorn

--- app.py
+++ app.py
@@ -185,4 +185,4 @@ if __name__ == "__main__":
     app.logger.setLevel(gunicorn_logger.level)
 
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(threaded=True, port=5000, debug=True)
