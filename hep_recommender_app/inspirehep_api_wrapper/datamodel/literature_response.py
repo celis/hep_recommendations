6a68eb4c1069d482bc4f4a1fff051deaed608568
celis <acelisalas@gmail.com>
2020-02-10 16:36:45 +0100

    add inspire wrapper

---
+++ hep_recommender_app/inspirehep_api_wrapper/datamodel/literature_response.py
@@ -0,0 +1,19 @@
+from requests import Response
+from hep_recommender_app.inspirehep_api_wrapper.datamodel.literature_record import LiteratureRecord
+from typing import Dict, Any
+
+
+class LiteratureResponse:
+    """
+    Contains the response from the literature endpoint
+    """
+
+    def __init__(self, api_literature_response: Response):
+        self.api_literature_response = api_literature_response
+
+    @property
+    def data(self) -> Dict[str, Any]:
+        return self.api_literature_response.json()
+
+    def to_record(self) -> LiteratureRecord:
+        return LiteratureRecord(self.data)
---
+++ hep_recommender_app/inspirehep_api_wrapper/service/inspire_api.py
@@ -0,0 +1,26 @@
+import requests
+from hep_recommender_app.inspirehep_api_wrapper.datamodel.literature_response import LiteratureResponse
+
+
+class InspireAPI:
+    """
+    Simple wrapper around the inspire api
+
+    methods:
+       literature: gives access to the literature endpoint
+    """
+
+    LITERATURE = "https://labs.inspirehep.net/api/literature/"
+
+    def __init__(self):
+        pass
+
+    def literature(self, record_id: str) -> LiteratureResponse:
+        """
+        Returns api response for a given record_id
+        """
+        url = self.LITERATURE + record_id
+        return LiteratureResponse(requests.get(url))
+
+    def data(self, record_id):
+        return {"id": record_id, "record": self.literature(record_id).to_record()}
